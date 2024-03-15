3aaabf2c0b6c7708afbb111285b4fe2de02291ca
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-15 11:47:23 -0300

    Passo 2

--- exercicios.js
+++ exercicios.js
@@ -1,5 +1,16 @@
 const array = [4,5,77,98,23,54];
 
-array.forEach((index,element) =>{
-    console.log(index,element);
+array.forEach((element, index) => {
+    console.log(element, index);
 })
+
+
+const executarOperacaoEmArray = (array, callback) => {
+    array.forEach((num) => callback(num));
+};
+
+const callback = (num) => {
+    console.log(num * 5);
+}
+
+executarOperacaoEmArray(array, callback);
